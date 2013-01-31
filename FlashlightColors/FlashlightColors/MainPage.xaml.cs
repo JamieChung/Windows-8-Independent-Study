5a621a5b5ff5b8ddba404658061275be2a80bcec
Jamie Chung <me@jamiechung.me>
2013-01-31 14:49:44 -0500

    Flashlight colors

---
+++ FlashlightColors/FlashlightColors/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using FlashlightColors.Resources;
+using System.Windows.Shapes;
+using System.Windows.Media;
+
+namespace FlashlightColors
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        private void BlueButton_Click(object sender, RoutedEventArgs e)
+        {
+            Flashlight.Fill = new SolidColorBrush(Colors.Blue);
+        }
+
+        private void OrangeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Flashlight.Fill = new SolidColorBrush(Colors.Orange);
+        }
+
+        private void RedButton_Click (object sender, RoutedEventArgs e)
+        {
+            Flashlight.Fill = new SolidColorBrush(Colors.Red);
+        }
+
+        private void WhiteButton_Click(object sender, RoutedEventArgs e)
+        {
+            Flashlight.Fill = new SolidColorBrush(Colors.White);
+        }
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
