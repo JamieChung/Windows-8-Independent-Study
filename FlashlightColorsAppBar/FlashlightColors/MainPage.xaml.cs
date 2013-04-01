26d324d640fed1c2be2f140277f4d03f2180a8d6
Jamie Chung <me@jamiechung.me>
2013-03-31 23:29:17 -0400

    Updating with tutorials for FlashlightColorsAppBar and MapApp

---
+++ FlashlightColorsAppBar/FlashlightColors/MainPage.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using FlashlightColors.Resources;
+using System.Windows.Media;
+
+namespace FlashlightColors
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            BuildLocalizedApplicationBar();
+        }
+
+        private void ColorButton_Click(object sender, RoutedEventArgs e)
+        {
+            var color = Colors.White;
+            var button = (Button)sender;
+
+            switch (button.Name)
+            {
+                case "WhiteButton":
+                    color = Colors.White;
+                    break;
+
+                case "BlueButton":
+                    color = Colors.Blue;
+                    break;
+
+                case "RedButton":
+                    color = Colors.Red;
+                    break;
+
+                case "OrangeButton":
+                    color = Colors.Orange;
+                    break;
+            }
+
+            Flashlight.Fill = new SolidColorBrush(color);
+        }
+
+        //Sample code for building a localized ApplicationBar
+        private void BuildLocalizedApplicationBar()
+        {
+            // Set the page's ApplicationBar to a new instance of ApplicationBar.
+            ApplicationBar = new ApplicationBar();
+
+            // Create a new menu item with the localized string from AppResources.
+            ApplicationBarMenuItem appBarMenuItem;
+
+            // Blue
+            appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarColorBlueText);
+            appBarMenuItem.Click += appBarMenuItem_Click;
+            ApplicationBar.MenuItems.Add(appBarMenuItem);
+
+            // White
+            appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarColorWhiteText);
+            appBarMenuItem.Click += appBarMenuItem_Click;
+            ApplicationBar.MenuItems.Add(appBarMenuItem);
+
+            // Red
+            appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarColorRedText);
+            appBarMenuItem.Click += appBarMenuItem_Click;
+            ApplicationBar.MenuItems.Add(appBarMenuItem);
+
+            // Orange
+            appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarColorOrangeText);
+            appBarMenuItem.Click += appBarMenuItem_Click;
+            ApplicationBar.MenuItems.Add(appBarMenuItem);
+
+        }
+
+        void appBarMenuItem_Click(object sender, EventArgs e)
+        {
+            var menuItem = (ApplicationBarMenuItem)sender;
+            var color = Colors.White;
+
+            // We use the .Text attribute instead of .Name because that is what
+            // ApplicationBarMenuItem supports
+            switch (menuItem.Text)
+            {
+                // Notice that we cannot use the AppResources.AppBarColorWhiteText
+                // This is because C# requires that switch statements use static constant strings
+                // Our strings using AppResources are not static by nature.
+                case "White":
+                    color = Colors.White;
+                    break;
+
+                case "Blue":
+                    color = Colors.Blue;
+                    break;
+
+                case "Red":
+                    color = Colors.Red;
+                    break;
+
+                case "Orange":
+                    color = Colors.Orange;
+                    break;
+            }
+
+            Flashlight.Fill = new SolidColorBrush(color);
+        }
+    }
+}
---
+++ MapApp/MapApp/MainPage.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using MapApp.Resources;
+
+using Microsoft.Phone.Maps.Controls;
+using System.Device.Location; // Provides the GeoCoordinate class.
+using Windows.Devices.Geolocation; //Provides the Geocoordinate class.
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace MapApp
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            BuildLocalizedApplicationBar();
+
+            ShowDeviceLocation();
+        }
+
+        private async void ShowDeviceLocation()
+        {
+            // Get current location from device
+            Geolocator deviceLocator = new Geolocator();
+            
+            // Sends an asynchronous request to get the device locationn
+            Geoposition devicePosition = await deviceLocator.GetGeopositionAsync();
+
+            // Converts the Geocoordinate from the device position to a GeoCoordinate
+            // Uses the helper class we just created: CoordinateConverter
+            GeoCoordinate deviceCoordinate = CoordinateConverter.ConvertGeocoordinate(devicePosition.Coordinate);
+
+            // Create a small circle to mark the current location.
+            Ellipse myCircle = new Ellipse();
+            myCircle.Fill = new SolidColorBrush(Colors.Blue);
+            myCircle.Height = 20;
+            myCircle.Width = 20;
+            myCircle.Opacity = 50;
+
+            // Create a MapOverlay to contain the circle.
+            MapOverlay myLocationOverlay = new MapOverlay();
+            myLocationOverlay.Content = myCircle;
+            myLocationOverlay.PositionOrigin = new Point(0.5, 0.5);
+            myLocationOverlay.GeoCoordinate = deviceCoordinate;
+
+            // Create a MapLayer to contain the MapOverlay.
+            MapLayer myLocationLayer = new MapLayer();
+            myLocationLayer.Add(myLocationOverlay);
+
+            // Add the MapLayer to the Map.
+            MapView.Layers.Add(myLocationLayer);
+
+            // Center the map and zoom in to map level 13 
+            MapView.Center = deviceCoordinate;
+            MapView.ZoomLevel = 13;
+
+        }
+
+
+        // Sample code for building a localized ApplicationBar
+        private void BuildLocalizedApplicationBar()
+        {
+            // Set the page's ApplicationBar to a new instance of ApplicationBar.
+            ApplicationBar = new ApplicationBar();
+
+            // Create a new menu item with the localized string from AppResources.
+            ApplicationBarMenuItem appBarMenuItem;
+
+            appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBar_ZoomIn);
+            appBarMenuItem.Click += ZoomIn_click;
+            ApplicationBar.MenuItems.Add(appBarMenuItem);
+
+            appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBar_ZoomOut);
+            appBarMenuItem.Click += ZoomOut_click;
+            ApplicationBar.MenuItems.Add(appBarMenuItem);
+
+            appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBar_AerialView);
+            appBarMenuItem.Click += AerialView_click;
+            ApplicationBar.MenuItems.Add(appBarMenuItem);
+
+            appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBar_RoadView);
+            appBarMenuItem.Click += RoadView_click;
+            ApplicationBar.MenuItems.Add(appBarMenuItem);
+        }
+
+        void ZoomIn_click(object sender, EventArgs e)
+        {
+            double zoom;
+            zoom = MapView.ZoomLevel;
+            MapView.ZoomLevel = ++zoom;
+        }
+
+        void ZoomOut_click(object sender, EventArgs e)
+        {
+            double zoom;
+            zoom = MapView.ZoomLevel;
+            MapView.ZoomLevel = --zoom;
+        }
+
+        void AerialView_click(object sender, EventArgs e)
+        {
+            MapView.CartographicMode = MapCartographicMode.Aerial;
+        }
+
+        void RoadView_click(object sender, EventArgs e)
+        {
+            MapView.CartographicMode = MapCartographicMode.Road;
+        }
+    }
+}
